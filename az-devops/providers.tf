7853ca634df3f1f1b3b4d87af77f9219d5ba5b04
D Pragathi <pragathi123@gmail.com>
2026-07-03 10:51:15 +0530

    Initial Terraform project

--- az-devops/providers.tf
+++ az-devops/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     azurerm = {
@@ -11,6 +11,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
   subscription_id = var.subscription_id
 }
